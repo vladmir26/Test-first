43d8e872ffc69d49214f77e9ae5283f46623d45c
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-08-23 17:56:19 +0300

    Made changes, corrected the pop-up of the form, corrected the layout of the form

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-/*const button = document.querySelector('.btn-primary');
-const form = document.querySelector('.d-none');
-
-button.addEventListener('click', () => {
-     form.classList.remove('d-none');
-     form.classList.add('d-flex', 'flex-column');
-});
-*/
-var exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
-  modalBodyInput.value = recipient
-})
-
 IMask(
      document.querySelector('.mask'),
      {
        mask: '+{7}(000)000-00-00'
      }
 ) 
+
+
+
